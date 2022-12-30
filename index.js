a152f28df1c2398924ff88208130151177779ca6
Pradyuman <iampradyuman18@gmail.com>
2022-12-30 21:19:19 +0530

    updated url

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "webfile-share.glitch.me";
+const baseURL = "https://webfile-share.glitch.me";
 const uploadURL = `${baseURL}/api/files`;
-const emailURL = `${baseURL}/api/files/send`;
+const emailURL = `${baseURL}/api/files/send`; 
 
 const maxAllowedSize = 100 * 1024 * 1024; //100mb
 
